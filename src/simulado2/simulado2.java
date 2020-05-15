56014a71809fb60734a9b6f4475feaa82016d6fd
BrenoEduardo <brenoeducm@hotmail.com>
2020-05-15 12:01:37 -0300

    Leitura de escolaridade

--- src/simulado2/simulado2.java
+++ src/simulado2/simulado2.java
@@ -1,9 +1,28 @@
 package simulado2;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class simulado2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int digi;
+		
+		// Leitura de escolaridade
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.printf("Digite uma opção: ");
+		digi = sc.nextInt();
+		
+		
+		
+		sc.close();
 
 	}
 
